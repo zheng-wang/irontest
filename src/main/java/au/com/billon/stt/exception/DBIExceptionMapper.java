5a31faef17a657d64a341c549c122190000af370
Trevor Li <trevorli77@hotmail.com>
2015-07-04 17:15:09 +0300

    optimise to make it universal

--- src/main/java/au/com/billon/stt/exception/DBIExceptionMapper.java
+++ src/main/java/au/com/billon/stt/exception/DBIExceptionMapper.java
@@ -1,16 +1,17 @@
 package au.com.billon.stt.exception;
 
+import io.dropwizard.jdbi.jersey.LoggingDBIExceptionMapper;
 import org.skife.jdbi.v2.exceptions.DBIException;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 /**
  * Created by Trevor Li on 7/4/15.
  */
 @Provider
-public class DBIExceptionMapper implements ExceptionMapper<DBIException> {
+public class DBIExceptionMapper extends LoggingDBIExceptionMapper {
 
+    @Override
     public Response toResponse(DBIException exception) {
         if (exception.getMessage().indexOf("Unique index or primary key violation") > -1) {
             return Response.status(Response.Status.CONFLICT).type("text/plain").entity("The name is already taken").build();
