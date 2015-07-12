1e3ec9a4dd244fa30258703d17c0aa935e4ff289
Zheng Wang <zhengzwang@hotmail.com>
2015-07-12 17:33:34 +0800

    cascade deleting test steps when deleting test case

--- src/main/java/au/com/billon/stt/db/TeststepDAO.java
+++ src/main/java/au/com/billon/stt/db/TeststepDAO.java
@@ -14,7 +14,8 @@ public interface TeststepDAO {
     @SqlUpdate("create table IF NOT EXISTS teststep (" +
             "id INT PRIMARY KEY auto_increment, testcase_id INT, name varchar(200), description clob, " +
             "created timestamp DEFAULT CURRENT_TIMESTAMP, updated timestamp DEFAULT CURRENT_TIMESTAMP, " +
-            "type varchar(20), request clob, FOREIGN KEY (testcase_id) REFERENCES testcase(id))")
+            "type varchar(20), request clob, " +
+            "FOREIGN KEY (testcase_id) REFERENCES testcase(id) ON DELETE CASCADE)")
     void createTableIfNotExists();
 
     @SqlUpdate("insert into teststep (testcase_id, name, type, description, request) values (:testcaseId, :name, :type, :description, :request)")
