2ac548a99371f5effdccf30468577aa4ce25fd6c
Trevor Li <trevorli77@hotmail.com>
2015-07-15 15:40:10 +0300

    endpoint properties

--- src/main/java/au/com/billon/stt/db/EndpointMapper.java
+++ src/main/java/au/com/billon/stt/db/EndpointMapper.java
@@ -13,7 +13,6 @@ import java.sql.SQLException;
 public class EndpointMapper implements ResultSetMapper<Endpoint> {
     public Endpoint map(int index, ResultSet rs, StatementContext ctx) throws SQLException {
         return new Endpoint(rs.getLong("id"), rs.getString("name"), rs.getString("description"), rs.getString("handler"),
-            rs.getString("url"), rs.getString("username"), rs.getString("password"),
             rs.getTimestamp("created"), rs.getTimestamp("updated"));
     }
 }
