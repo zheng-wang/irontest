c26ea4e92f281e03ba7b2031b9d4a5e8dafdda68
Zheng Wang <zhengzwang@hotmail.com>
2015-07-20 23:49:44 +0800

    fix the successfulMessage directive

--- src/main/resources/assets/app/js/app-directives.js
+++ src/main/resources/assets/app/js/app-directives.js
@@ -5,7 +5,11 @@ angular.module('service-testing-tool').directive('successfulMessage', function (
         link: function(scope, element, attrs) {
             var el = element[0];
             el.addEventListener("animationend", function() {
-                scope.savingStatus.saveSuccessful = null;
+                if (scope.savingStatus) {
+                  scope.savingStatus.saveSuccessful = null;
+                } else {
+                  scope.saveSuccessful = null;
+                }
             });
             angular.element(el).addClass('successful-message');
         }
