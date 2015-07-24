b8d43be0c394b57014f111820f78706b84c5685d
Trevor Li <trevorli77@hotmail.com>
2015-07-24 17:07:05 +0300

    Testrun class and Soap handler

--- src/main/java/au/com/billon/stt/resources/TeststepResource.java
+++ src/main/java/au/com/billon/stt/resources/TeststepResource.java
@@ -1,17 +1,11 @@
 package au.com.billon.stt.resources;
 
-import au.com.billon.stt.Utils;
 import au.com.billon.stt.db.TeststepDAO;
-import au.com.billon.stt.models.SOAPTeststepInvocationRequestProperties;
 import au.com.billon.stt.models.Teststep;
-import au.com.billon.stt.models.TeststepInvocationRequest;
-import au.com.billon.stt.models.TeststepInvocationResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.reficio.ws.client.core.SoapClient;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import javax.xml.transform.TransformerException;
 
 /**
  * Created by Zheng on 11/07/2015.
@@ -47,22 +41,5 @@ public class TeststepResource {
     @DELETE @Path("{teststepId}")
     public void delete(@PathParam("teststepId") long teststepId) {
         dao.deleteById(teststepId);
-    }
-
-    // This is not a REST service. It is actually an RPC through JSON.
-    // It is implemented for simplicity for now.
-    @POST @Path("{teststepId}/invoke")
-    public TeststepInvocationResponse invoke(TeststepInvocationRequest invocationRequest) throws TransformerException {
-        TeststepInvocationResponse invocationResponse = null;
-
-        if (TeststepInvocationRequest.TESTSTEP_INVOCATION_TYPE_SOAP.equals(invocationRequest.getType())) {
-            SOAPTeststepInvocationRequestProperties properties =
-                    (SOAPTeststepInvocationRequestProperties) invocationRequest.getProperties();
-            SoapClient client = SoapClient.builder().endpointUri(properties.getSoapAddress()).build();
-            String response = client.post(invocationRequest.getRequest());
-            invocationResponse = new TeststepInvocationResponse(Utils.prettyPrintXML(response));
-        }
-
-        return invocationResponse;
     }
 }
