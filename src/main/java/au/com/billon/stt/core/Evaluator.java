5dc734d77c9d8581bdd52924d7146eb593c0ee93
Zheng Wang <zhengzwang@hotmail.com>
2015-08-02 12:50:23 +0800

    xpath evaluation - support boolean, number and string value

--- src/main/java/au/com/billon/stt/core/Evaluator.java
+++ src/main/java/au/com/billon/stt/core/Evaluator.java
@@ -4,11 +4,13 @@ import au.com.billon.stt.models.EvaluationRequest;
 import au.com.billon.stt.models.EvaluationResponse;
 import au.com.billon.stt.models.XPathEvaluationRequestProperties;
 import au.com.billon.stt.utils.XMLUtils;
+import com.sun.org.apache.xpath.internal.XPathException;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.io.StringReader;
 
@@ -22,21 +24,40 @@ public class Evaluator {
             XPathEvaluationRequestProperties properties = (XPathEvaluationRequestProperties) request.getProperties();
             XPath xpath = XPathFactory.newInstance().newXPath();
             xpath.setNamespaceContext(new STTNamespaceContext(properties.getNamespacePrefixes()));
-            InputSource inputSource = new InputSource(new StringReader(request.getInput()));
-            NodeList nodeList = null;
+
             String result = null;
+            boolean error = false;
             try {
-                nodeList = (NodeList) xpath.evaluate(request.getExpression(), inputSource, XPathConstants.NODESET);
-                result = XMLUtils.domNodeListToString(nodeList);
+                InputSource inputSource = new InputSource(new StringReader(request.getInput()));
+                Object value = xpath.evaluate(request.getExpression(), inputSource, XPathConstants.NODESET);
+                result = XMLUtils.domNodeListToString((NodeList) value);
+            } catch (XPathExpressionException e) {
+                Throwable cause = e.getCause();
+                if (cause instanceof XPathException &&
+                        cause.getMessage().startsWith("Can not convert") && cause.getMessage().endsWith("!")) {
+                    //  The value is not of type NODESET. Swallow the exception and try STRING.
+                    InputSource inputSource2 = new InputSource(new StringReader(request.getInput()));
+                    try {
+                        result = (String) xpath.evaluate(request.getExpression(), inputSource2, XPathConstants.STRING);
+                    } catch (XPathExpressionException e1) {
+                        e.printStackTrace();
+                        result = e.getMessage();
+                        error = true;
+                    }
+                } else {
+                    e.printStackTrace();
+                    result = e.getMessage();
+                    error = true;
+                }
             } catch (Exception e) {
                 e.printStackTrace();
                 result = e.getMessage();
-                response.setError(true);
+                error = true;
             }
 
             response.setResult(result);
+            response.setError(error);
         }
         return response;
     }
-
 }
