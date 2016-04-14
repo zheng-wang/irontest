2dd9238f8232274ec79e44d2f68b73b6be0af6ab
Zheng Wang <zhengzwang@hotmail.com>
2016-04-14 21:19:03 +0800

    remove interface from DB test step

--- src/main/resources/assets/app/js/teststeps/db-teststep-creation-controller.js
+++ src/main/resources/assets/app/js/teststeps/db-teststep-creation-controller.js
@@ -1,21 +1,15 @@
 'use strict';
 
-angular.module('iron-test').controller('DBTeststepCreationController', ['$scope', 'Teststeps',
-    '$location', '$stateParams', '$state', 'PageNavigation',
-  function($scope, Teststeps, $location, $stateParams, $state, PageNavigation) {
-    $scope.teststep = {};
-
+angular.module('iron-test').controller('DBTeststepCreationController', ['$scope', 'Teststeps', '$state', '$stateParams',
+  function($scope, Teststeps, $state, $stateParams) {
     $scope.create = function(isValid) {
       if (isValid) {
         var teststep = new Teststeps({
-          testcaseId: this.teststep.testcaseId,
-          name: this.teststep.name,
-          description: this.teststep.description,
+          testcaseId: $stateParams.testcaseId,
+          name: this.name,
+          description: this.description,
           type: 'DB'
         });
-        if (this.teststep.intfaceId) {
-          teststep.intfaceId = this.teststep.intfaceId;
-        }
         teststep.$save(function(response) {
           $state.go('teststep_edit', {testcaseId: response.testcaseId, teststepId: response.id});
         }, function(error) {
@@ -23,39 +17,6 @@ angular.module('iron-test').controller('DBTeststepCreationController', ['$scope'
         });
       } else {
         $scope.submitted = true;
-      }
-    };
-
-    $scope.goto = function(state, params, expect) {
-      var context = {
-        model: $scope.teststep,
-        url: $location.path(),
-        expect: expect
-      };
-
-      PageNavigation.contexts.push(context);
-
-      $state.go(state, params);
-    };
-
-    $scope.findOne = function() {
-      // entry returned from other pages
-      var model = PageNavigation.returns.pop();
-      if (model) {
-        $scope.teststep = model;
-      } else {
-        if ($stateParams.teststepId) {
-          // edit an existing entry
-          Teststeps.get({
-            testcaseId: $stateParams.testcaseId,
-            teststepId: $stateParams.teststepId
-          }, function (response) {
-            $scope.teststep = response;
-          });
-        } else {
-          // create a new entry
-          $scope.teststep.testcaseId = $stateParams.testcaseId;
-        }
       }
     };
   }
