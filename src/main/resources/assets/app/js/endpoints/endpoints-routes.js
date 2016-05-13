846c2060845fc013fca91ef619dbb067dbe805f9
Zheng Wang <zhengzwang@hotmail.com>
2016-05-13 21:09:40 +0800

    remove db endpoint creation form

--- src/main/resources/assets/app/js/endpoints/endpoints-routes.js
+++ src/main/resources/assets/app/js/endpoints/endpoints-routes.js
@@ -2,10 +2,6 @@
 
 angular.module('iron-test').config(['$stateProvider', function ($stateProvider) {
   $stateProvider
-    .state('endpoint_create_db', {
-        url: '/environments/:environmentId/endpoints/createDBEndpoint',
-        templateUrl: '/ui/views/endpoints/db/create-db-endpoint.html'
-    })
     .state('endpoint_edit', {
         url: '/environments/:environmentId/endpoints/:endpointId/edit',
         params: {
