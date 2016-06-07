d6ed3bc982a5123b973121147cb6bc756e6b06ed
Zheng Wang <zheng-wang@users.noreply.github.com>
2016-06-07 21:19:45 +0800

    read body when dequeue

--- src/main/java/io/irontest/core/runner/MQTeststepRunner.java
+++ src/main/java/io/irontest/core/runner/MQTeststepRunner.java
@@ -2,6 +2,8 @@ package io.irontest.core.runner;
 
 import com.ibm.mq.*;
 import com.ibm.mq.constants.CMQC;
+import com.ibm.mq.headers.MQDataException;
+import com.ibm.mq.headers.MQHeaderIterator;
 import io.irontest.models.MQIIBEndpointProperties;
 import io.irontest.models.MQTeststepProperties;
 import io.irontest.models.Teststep;
@@ -13,7 +15,7 @@ import java.util.Hashtable;
  * Created by Trevor Li on 7/14/15.
  */
 public class MQTeststepRunner implements TeststepRunner {
-    public Object run(Teststep teststep) throws MQException, IOException {
+    public Object run(Teststep teststep) throws MQException, IOException, MQDataException {
         Object result = null;
         MQIIBEndpointProperties endpointProperties = (MQIIBEndpointProperties) teststep.getEndpoint().getOtherProperties();
         MQTeststepProperties teststepProperties = (MQTeststepProperties) teststep.getOtherProperties();
@@ -55,14 +57,15 @@ public class MQTeststepRunner implements TeststepRunner {
         return result;
     }
 
-    private String dequeue(MQQueue queue) throws MQException, IOException {
+    private String dequeue(MQQueue queue) throws MQException, IOException, MQDataException {
         String result = null;
         MQGetMessageOptions getOptions = new MQGetMessageOptions();
         getOptions.options = CMQC.MQGMO_NO_WAIT + CMQC.MQGMO_FAIL_IF_QUIESCING;
         MQMessage message = new MQMessage();
         try {
             queue.get(message, getOptions);
-            result = message.readStringOfByteLength(message.getDataLength());
+            MQHeaderIterator it = new MQHeaderIterator(message);
+            result = it.getBodyAsText();
         } catch(MQException mqEx) {
             if (mqEx.getCompCode() == CMQC.MQCC_FAILED && mqEx.getReason() == CMQC.MQRC_NO_MSG_AVAILABLE) {
                 //  No more message available on the queue
