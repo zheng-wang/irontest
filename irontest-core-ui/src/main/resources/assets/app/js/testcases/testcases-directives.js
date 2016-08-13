2a30a785d220c2cdefa45e47268aab30ad2be1e7
Zheng Wang <zheng-wang@users.noreply.github.com>
2016-08-13 23:32:07 +0800

    refine code

--- irontest-core-ui/src/main/resources/assets/app/js/testcases/testcases-directives.js
+++ irontest-core-ui/src/main/resources/assets/app/js/testcases/testcases-directives.js
@@ -1,16 +1,13 @@
 'use strict';
 
-angular.module('irontest').directive('selectedStepRunReportArea', function () {
+angular.module('irontest').directive('selectedStepRunReportArea', function ($timeout) {
   return {
     link: function(scope, element, attrs) {
-      //  use scope watch to ensure teststepGrid is completed loaded.
-      scope.$watch('testcaseRun.selectedStepRunReport', function (stepRunReport) {
-        if (stepRunReport) {
-          var gridRect = document.getElementById('teststepGrid').getBoundingClientRect();
-          var height = (window.innerHeight - gridRect.bottom) * 0.9;
-          element.height(height);
-        }
-      })
+      $timeout(function() {   //  wait for teststepGrid to be completely loaded
+        var gridRect = document.getElementById('teststepGrid').getBoundingClientRect();
+        var height = (window.innerHeight - gridRect.bottom) * 0.9;
+        element.height(height);
+      });
     }
   };
 });
