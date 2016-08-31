040f45ca630a646bc8bb965e51fc9f63b53a5f72
Zheng Wang <zheng-wang@users.noreply.github.com>
2016-08-31 22:12:19 +0800

    folder based test case management (3)

--- irontest-core-ui/src/main/resources/assets/app/js/foldertree/foldertree-controller.js
+++ irontest-core-ui/src/main/resources/assets/app/js/foldertree/foldertree-controller.js
@@ -9,27 +9,30 @@ angular.module('irontest').controller('FolderTreeController', ['$scope',
         },
         check_callback: true
       },
-      plugins : [ 'contextmenu' ],
-      version : 1          //  ngJsTree property
+      plugins: [ 'contextmenu' ],
+      version: 1          //  ngJsTree property
     };
 
     $scope.treeData = [
-      { id : '1', parent : '#', text : 'Root', state: { opened: true} },
-      { id : '2', parent : '1', text : 'Child 1', state: { opened: true} },
-      { id : '3', parent : '1', text : 'Child 2' , state: { opened: true}}
+      { id: '1', parent: '#', text: 'Root', state: { opened: true} },
+      { id: '2', parent: '1', text: 'Child 1', state: { opened: true} },
+      { id: '3', parent: '1', text: 'Child 2 erewradfdsfasfasdfasdfasdfasdfasfdafdfasfaf', state: { opened: true }}
     ];
+    for (var i = 3; i < 40; i++) {
+      $scope.treeData.push({ id: '' + (i + 1), parent: '1', text: 'Child ' + i, state: { opened: true }});
+    }
 
     var createNodeCB = function(e, item) {
       console.log('Added new node with the text ' + item.node.text);
     };
 
-    var readyCB = function() {
+    /*var readyCB = function() {
       // tree is ready and $scope.treeInstance is ready for use
       console.log('JS Tree Ready');
-    };
+    };*/
 
     $scope.treeEventsObj = {
-      ready: readyCB,
+      //ready: readyCB,
       create_node: createNodeCB
     }
   }
