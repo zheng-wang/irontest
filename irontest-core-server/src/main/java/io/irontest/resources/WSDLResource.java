c4e7748f4b5c58baba764bcda80bd0e5788e0797
Zheng Wang <zheng-wang@users.noreply.github.com>
2017-08-03 18:34:46 +0800

    refine code

--- irontest-core-server/src/main/java/io/irontest/resources/WSDLResource.java
+++ irontest-core-server/src/main/java/io/irontest/resources/WSDLResource.java
@@ -1,17 +1,21 @@
 package io.irontest.resources;
 
+import com.predic8.wsdl.Binding;
+import com.predic8.wsdl.BindingOperation;
+import com.predic8.wsdl.Definitions;
+import com.predic8.wsdl.WSDLParser;
+import com.predic8.wstool.creator.RequestTemplateCreator;
+import com.predic8.wstool.creator.SOARequestCreator;
+import groovy.xml.MarkupBuilder;
 import io.irontest.models.WSDLBinding;
 import io.irontest.models.teststep.SOAPOperationInfo;
-import org.reficio.ws.builder.SoapBuilder;
-import org.reficio.ws.builder.SoapOperation;
-import org.reficio.ws.builder.core.Wsdl;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.xml.namespace.QName;
+import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,16 +30,14 @@ public class WSDLResource {
     @GET @Path("/{wsdlUrl}/bindings")
     public List<WSDLBinding> getWSDLBindings(@PathParam("wsdlUrl") String wsdlUrl) throws UnsupportedEncodingException {
         List<WSDLBinding> result = new ArrayList<WSDLBinding>();
-        Wsdl wsdl = Wsdl.parse(wsdlUrl);
-        List<QName> bindings = wsdl.getBindings();
-        for (QName binding: bindings) {
-            SoapBuilder builder = wsdl.getBuilder(binding);
-            List<SoapOperation> operations = builder.getOperations();
+        WSDLParser parser = new WSDLParser();
+        Definitions definition = parser.parse(wsdlUrl);
+        for (Binding binding: definition.getBindings()) {
             List<String> operationNames = new ArrayList<String>();
-            for (SoapOperation operation: operations) {
-                operationNames.add(operation.getOperationName());
+            for (BindingOperation operation: binding.getOperations()) {
+                operationNames.add(operation.getName());
             }
-            result.add(new WSDLBinding(binding.getLocalPart(), operationNames));
+            result.add(new WSDLBinding(binding.getName(), operationNames));
         }
 
         return result;
@@ -45,10 +47,13 @@ public class WSDLResource {
     public SOAPOperationInfo getOperationInfo(@PathParam("wsdlUrl") String wsdlUrl, @PathParam("bindingName") String bindingName,
                                               @PathParam("operationName") String operationName) {
         SOAPOperationInfo info = new SOAPOperationInfo();
-        Wsdl wsdl = Wsdl.parse(wsdlUrl);
-        SoapBuilder builder = wsdl.binding().localPart(bindingName).find();
-        SoapOperation operation = builder.operation().name(operationName).find();
-        info.setSampleRequest(builder.buildInputMessage(operation));
+        WSDLParser parser = new WSDLParser();
+        Definitions definition = parser.parse(wsdlUrl);
+        StringWriter writer = new StringWriter();
+        SOARequestCreator creator = new SOARequestCreator(definition, new RequestTemplateCreator(), new MarkupBuilder(writer));
+        creator.createRequest(null, operationName, bindingName);
+        info.setSampleRequest(writer.toString());
+
         return info;
     }
 }
