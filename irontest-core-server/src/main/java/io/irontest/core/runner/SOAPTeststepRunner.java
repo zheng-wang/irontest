a2e1107bf6a85ece375b541e7064a5da70b3371e
Zheng Wang <zheng-wang@users.noreply.github.com>
2017-08-04 18:30:32 +0800

    refine SOAP test step security

--- irontest-core-server/src/main/java/io/irontest/core/runner/SOAPTeststepRunner.java
+++ irontest-core-server/src/main/java/io/irontest/core/runner/SOAPTeststepRunner.java
@@ -4,25 +4,27 @@ import io.irontest.models.endpoint.Endpoint;
 import io.irontest.models.teststep.HTTPHeader;
 import io.irontest.models.teststep.SOAPTeststepProperties;
 import io.irontest.models.teststep.Teststep;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.SSLContext;
 import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 /**
  * Created by Trevor Li on 7/14/15.
@@ -34,17 +36,6 @@ public class SOAPTeststepRunner extends TeststepRunner {
         BasicTeststepRun basicTeststepRun = new BasicTeststepRun();
         Endpoint endpoint = teststep.getEndpoint();
 
-        //  build http client instance
-        HttpClientBuilder httpClientBuilder = HttpClients.custom();
-        if (!"".equals(StringUtils.trimToEmpty(endpoint.getUsername()))) {
-            CredentialsProvider provider = new BasicCredentialsProvider();
-            UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(
-                    endpoint.getUsername(), endpoint.getPassword());
-            provider.setCredentials(AuthScope.ANY, credentials);
-            httpClientBuilder.setDefaultCredentialsProvider(provider);
-        }
-        CloseableHttpClient httpclient = httpClientBuilder.build();
-
         //  set request HTTP headers
         HttpPost httpPost = new HttpPost(endpoint.getUrl());
         SOAPTeststepProperties otherProperties = (SOAPTeststepProperties) teststep.getOtherProperties();
@@ -52,6 +43,13 @@ public class SOAPTeststepRunner extends TeststepRunner {
             for (HTTPHeader httpHeader : otherProperties.getHttpHeaders()) {
                 httpPost.setHeader(httpHeader.getName(), httpHeader.getValue());
             }
+        }
+        //  set HTTP basic auth
+        if (!"".equals(StringUtils.trimToEmpty(endpoint.getUsername()))) {
+            String auth = endpoint.getUsername() + ":" + endpoint.getPassword();
+            String encodedAuth = Base64.encodeBase64String(auth.getBytes());
+            String authHeader = "Basic " + encodedAuth;
+            httpPost.setHeader(HttpHeaders.AUTHORIZATION, authHeader);
         }
 
         //  set request HTTP body
@@ -72,7 +70,18 @@ public class SOAPTeststepRunner extends TeststepRunner {
                 return null;
             }
         };
-        httpclient.execute(httpPost, responseHandler);
+
+        //  build HTTP Client instance
+        //  trust all SSL certificates
+        SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(new TrustStrategy() {
+            public boolean isTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                return true;
+            }
+        }).build();
+        HttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
+
+        //  invoke the web service
+        httpClient.execute(httpPost, responseHandler);
 
         basicTeststepRun.setResponse(apiResponse);
 
