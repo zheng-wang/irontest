5becf526fde39e82825453d9e62dc2034fd64ff3
Zheng Wang <zheng-wang@users.noreply.github.com>
2018-06-29 22:20:30 +0800

    prepare build for Maven Central

--- irontest-core-server/src/main/java/io/irontest/core/runner/RetainingColumnOrderResultSetMapper.java
+++ irontest-core-server/src/main/java/io/irontest/core/runner/RetainingColumnOrderResultSetMapper.java
@@ -10,13 +10,13 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * Keep the entries of the returned Map<String, Object> in the same order as the column names obtained from JDBC ResultSet metadata.
+ * Keep the entries of the returned {@link Map} in the same order as the column names obtained from JDBC ResultSet metadata.
  * Can't return ResultSetMetaData as it is inaccessible after ResultSet.close() (at least for H2).
  */
 public class RetainingColumnOrderResultSetMapper implements RowMapper<Map<String, Object>> {
     @Override
-    public Map<String, Object> map(ResultSet rs, StatementContext ctx) throws SQLException {
-        Map<String, Object> orderedRow = new LinkedHashMap<String, Object>();
+    public Map<String, Object> map(ResultSet rs, StatementContext ctx) {
+        Map<String, Object> orderedRow = new LinkedHashMap<>();
         try {
             ResultSetMetaData resultSetMetaData = rs.getMetaData();
             for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++) {
