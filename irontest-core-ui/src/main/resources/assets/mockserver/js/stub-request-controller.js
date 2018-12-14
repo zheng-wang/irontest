8c239af99b4427727d8c206921076a6f45eb1628
Zheng Wang <zheng-wang@users.noreply.github.com>
2018-12-14 21:25:33 +0800

    HTTP stubs

--- irontest-core-ui/src/main/resources/assets/mockserver/js/stub-request-controller.js
+++ irontest-core-ui/src/main/resources/assets/mockserver/js/stub-request-controller.js
@@ -6,6 +6,16 @@ angular.module('mockserver').controller('StubRequestController', ['$scope', 'Moc
     $scope.find = function() {
       MockServer.findStubRequestById({ stubRequestId: $stateParams.stubRequestId }, function(stubRequest) {
         $scope.stubRequest = stubRequest;
+
+        //  create formatted request headers string
+        var requestHeaders = stubRequest.request.headers;
+        $scope.stubRequestHeadersStr = '';
+        Object.keys(requestHeaders).forEach(function(key, index) {
+          if (index > 0) {
+            $scope.stubRequestHeadersStr += '\n';
+          }
+          $scope.stubRequestHeadersStr += key + ': ' + requestHeaders[key];
+        });
       }, function(response) {
         IronTestUtils.openErrorHTTPResponseModal(response);
       });
