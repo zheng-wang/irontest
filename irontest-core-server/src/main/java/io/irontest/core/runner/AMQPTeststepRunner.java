3d5e3475698d2b60af3f4ad489faa105a7d7c346
Zheng Wang <zheng-wang@users.noreply.github.com>
2019-04-29 21:36:01 +0800

    AMQP send action error handling

--- irontest-core-server/src/main/java/io/irontest/core/runner/AMQPTeststepRunner.java
+++ irontest-core-server/src/main/java/io/irontest/core/runner/AMQPTeststepRunner.java
@@ -1,28 +1,71 @@
 package io.irontest.core.runner;
 
-import com.ibm.mqlight.api.CompletionListener;
-import com.ibm.mqlight.api.NonBlockingClient;
-import com.ibm.mqlight.api.NonBlockingClientAdapter;
+import com.ibm.mqlight.api.*;
 import io.irontest.models.teststep.AMQPTeststepProperties;
 import io.irontest.models.teststep.Teststep;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AMQPTeststepRunner extends TeststepRunner {
-    protected BasicTeststepRun run(Teststep teststep) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AMQPTeststepRunner.class);
+
+    protected BasicTeststepRun run(Teststep teststep) throws Exception {
         AMQPTeststepProperties otherProperties = (AMQPTeststepProperties) teststep.getOtherProperties();
 
-        NonBlockingClient.create(teststep.getEndpoint().getUrl(), new NonBlockingClientAdapter<Void>() {
+        List<Exception> exceptionListDuringSending = new ArrayList<>();
+
+        NonBlockingClient theClient = NonBlockingClient.create(teststep.getEndpoint().getUrl(), new NonBlockingClientAdapter<Void>() {
             public void onStarted(NonBlockingClient client, Void context) {
                 client.send(otherProperties.getNodeAddress(), (String) teststep.getRequest(), null, new CompletionListener() {
                     public void onSuccess(NonBlockingClient client, Object context) {
                         client.stop(null, null);
                     }
                     public void onError(NonBlockingClient client, Object context, Exception exception) {
+                        Exception wrappedException = new Exception(
+                                "NonBlockingClient is started, but failed to send message to AMQP service. " +
+                                        exception.getMessage(), exception);
+                        LOGGER.error("", wrappedException);
+                        exceptionListDuringSending.add(wrappedException);
                         client.stop(null, null);
-                        throw new RuntimeException("Failed to send message to AMQP endpoint.", exception);
                     }
                 }, null);
             }
+
+            @Override
+            public void onRetrying(NonBlockingClient client, Void context, ClientException clientException) {
+                if (clientException != null) {
+                    Exception wrappedException = new Exception(
+                            "Unhandled exception in NonBlockingClientAdapter.onRetrying() method. " +
+                                    clientException.getMessage(), clientException);
+                    LOGGER.error("", wrappedException);
+                    exceptionListDuringSending.add(wrappedException);
+                    client.stop(null, null);
+                }
+            }
+
+            @Override
+            public void onStopped(NonBlockingClient client, Void context, ClientException clientException) {
+                if (clientException != null) {
+                    Exception wrappedException = new Exception(
+                            "Unhandled exception in NonBlockingClientAdapter.onStopped() method. "  +
+                                    clientException.getMessage(), clientException);
+                    LOGGER.error("", wrappedException);
+                    exceptionListDuringSending.add(wrappedException);
+                }
+            }
         }, null);
+
+        while (theClient.getState() != ClientState.STOPPED) {
+            LOGGER.info("Waiting for the AMQP client to stop ...");
+            Thread.sleep(1000);
+        }
+
+        if (!exceptionListDuringSending.isEmpty()) {
+            throw exceptionListDuringSending.get(0);    //  throw the first encountered exception
+        }
 
         return new BasicTeststepRun();
     }
