5c22247244178d855fe4ab88f90ca45bcea97802
Zheng Wang <zheng-wang@users.noreply.github.com>
2020-05-23 16:52:01 +0800

    refine code

--- irontest-core-server/src/main/java/io/irontest/db/VersionDAO.java
+++ irontest-core-server/src/main/java/io/irontest/db/VersionDAO.java
@@ -1,6 +1,6 @@
 package io.irontest.db;
 
-import io.irontest.Version;
+import io.irontest.Version;       //  compilation in IDE relies on the Maven generated Version.java under target/generated-sources/java-templates
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 public interface VersionDAO {
