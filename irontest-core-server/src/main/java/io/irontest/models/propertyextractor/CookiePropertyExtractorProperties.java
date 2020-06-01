0d7e80e878927bf3deffeea469f86bb0ff59c58f
Zheng Wang <zheng-wang@users.noreply.github.com>
2020-06-01 11:53:14 +0800

    add cookie property extractor

--- irontest-core-server/src/main/java/io/irontest/models/propertyextractor/CookiePropertyExtractorProperties.java
+++ irontest-core-server/src/main/java/io/irontest/models/propertyextractor/CookiePropertyExtractorProperties.java
@@ -1,7 +1,10 @@
 package io.irontest.models.propertyextractor;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import io.irontest.models.Properties;
+import io.irontest.resources.ResourceJsonViews;
 
+@JsonView({ResourceJsonViews.TestcaseExport.class})
 public class CookiePropertyExtractorProperties extends Properties {
     private String cookieName;
 
