942071abcaf6724e3cc3860cec96526ed8ccb73b
Zheng Wang <zheng-wang@users.noreply.github.com>
2020-06-07 12:34:55 +0800

    refine code

--- irontest-assembly/files/start-team.bat
+++ irontest-assembly/files/start-team.bat
@@ -1,3 +1,2 @@
-cd %~dp0
-FOR /F %%i IN ('dir /b /on irontest-*.jar ^| findStr /v "\-SNAPSHOT\.jar$"') DO SET newestJarFile=%%i
-java -Djava.net.useSystemProxies=true -Ddw.mode=team -jar %newestJarFile% server config.yml
+@echo off
+start.bat team
--- irontest-assembly/files/start.bat
+++ irontest-assembly/files/start.bat
@@ -1,3 +1,27 @@
+@echo off
 cd %~dp0
-FOR /F %%i IN ('dir /b /on irontest-*.jar ^| findStr /v "\-SNAPSHOT\.jar$"') DO SET newestJarFile=%%i
-java -Djava.net.useSystemProxies=true -jar %newestJarFile% server config.yml
+
+set count=0
+for /f %%i in ('dir /b /on irontest-*.jar 2^>nul') do (
+  set /a count+=1
+  set jarFile=%%i
+)
+
+if "%~1"=="team" (
+  set batFileName=start-team
+  set javaCommand=java -Djava.net.useSystemProxies=true -Ddw.mode=team -jar %jarFile% server config.yml
+) else (
+  set batFileName=start
+  set javaCommand=java -Djava.net.useSystemProxies=true -jar %jarFile% server config.yml
+)
+
+if %count% EQU 0 (
+  echo Iron Test jar file not found. & pause
+) else (
+  if %count% GTR 1 (
+    echo Multiple Iron Test jar files found. Please leave only one and rerun the %batFileName% command. & pause
+  ) else (
+    echo %javaCommand%
+    %javaCommand%
+  )
+)
