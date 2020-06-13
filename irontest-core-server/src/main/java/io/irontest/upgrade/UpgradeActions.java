66aa538340dc1614461bb36ba01f98dd263124f4
Zheng Wang <zheng-wang@users.noreply.github.com>
2020-06-13 19:27:55 +0800

    automate some upgrade steps

--- irontest-core-server/src/main/java/io/irontest/upgrade/UpgradeActions.java
+++ irontest-core-server/src/main/java/io/irontest/upgrade/UpgradeActions.java
@@ -1,22 +1,75 @@
 package io.irontest.upgrade;
 
+import io.dropwizard.db.DataSourceFactory;
+import io.irontest.IronTestConfiguration;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.reflections.Reflections;
 import org.reflections.scanners.ResourcesScanner;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import java.util.regex.Pattern;
 
 public class UpgradeActions {
-    public boolean needsSystemDBUpgrade(DefaultArtifactVersion oldVersion, DefaultArtifactVersion newVersion) {
+    private static Logger LOGGER = Logger.getLogger("Upgrade");
+
+    protected void upgrade(DefaultArtifactVersion systemDatabaseVersion, DefaultArtifactVersion jarFileVersion,
+                           IronTestConfiguration configuration) throws IOException {
+        boolean needsSystemDBUpgrade = needsSystemDBUpgrade(systemDatabaseVersion, jarFileVersion);
+        if (needsSystemDBUpgrade) {
+            System.out.println("Please manually backup <IronTest_Home>/database folder to your normal maintenance backup location. Type y and then Enter to confirm backup completion.");
+            Scanner scanner = new Scanner(System.in);
+            String line = null;
+            while (!"y".equalsIgnoreCase(line)) {
+                line = scanner.nextLine().trim();
+            }
+        }
+        boolean needsConfigYmlUpgrade = needsConfigYmlUpgrade(systemDatabaseVersion, jarFileVersion);
+        if (needsConfigYmlUpgrade) {
+            System.out.println("Please manually backup <IronTest_Home>/config.yml file to your normal maintenance backup location. Type y and then Enter to confirm backup completion.");
+            Scanner scanner = new Scanner(System.in);
+            String line = null;
+            while (!"y".equalsIgnoreCase(line)) {
+                line = scanner.nextLine().trim();
+            }
+        }
+
+        Path upgradeWorkspace = Files.createTempDirectory("irontest-upgrade");
+        Path logFilePath = Paths.get(upgradeWorkspace.toString(),
+                "upgrade-from-v" + systemDatabaseVersion + "-to-v" + jarFileVersion + ".log");
+        FileHandler logFileHandler = new FileHandler(logFilePath.toString());
+        logFileHandler.setFormatter(new SimpleFormatter());
+        LOGGER.addHandler(logFileHandler);
+        LOGGER.info("Create temp upgrade directory " + upgradeWorkspace.toString());
+
+        if (needsSystemDBUpgrade || needsConfigYmlUpgrade) {
+            Path oldDir = Paths.get(upgradeWorkspace.toString(), "old");
+            Path newDir = Paths.get(upgradeWorkspace.toString(), "new");
+            Files.createDirectory(oldDir);
+            Files.createDirectory(newDir);
+
+            if (needsSystemDBUpgrade) {
+                upgradeSystemDB(configuration.getSystemDatabase(), oldDir, newDir);
+            }
+        }
+    }
+
+    private boolean needsSystemDBUpgrade(DefaultArtifactVersion oldVersion, DefaultArtifactVersion newVersion) {
         Set<String> systemDBUpgradeFilePaths = getApplicableUpgradeFilePaths(oldVersion, newVersion, "db",
                 "SystemDB", "sql");
 
         return !systemDBUpgradeFilePaths.isEmpty();
     }
 
-    public boolean needsConfigYmlUpgrade(DefaultArtifactVersion oldVersion, DefaultArtifactVersion newVersion) {
+    private boolean needsConfigYmlUpgrade(DefaultArtifactVersion oldVersion, DefaultArtifactVersion newVersion) {
         Set<String> configYmlUpgradeFilePaths = getApplicableUpgradeFilePaths(oldVersion, newVersion,
                 "configyml", "ConfigYml", "class");
 
@@ -48,5 +101,23 @@ public class UpgradeActions {
         }
 
         return result;
+    }
+
+    private void upgradeSystemDB(DataSourceFactory systemDBConfiguration, Path oldDir, Path newDir) throws IOException {
+        Path oldDatabaseFolder = Files.createDirectory(Paths.get(oldDir.toString(), "database"));
+        Path newDatabaseFolder = Files.createDirectory(Paths.get(newDir.toString(), "database"));
+
+        String systemDBURL = systemDBConfiguration.getUrl();
+        String systemDBBaseURL = systemDBURL.split(";")[0];
+        String systemDBRelativePath = systemDBBaseURL.replace("jdbc:h2:", "");
+        String[] systemDBFileRelativePathFragments = systemDBRelativePath.split("/");
+        String systemDBFileName = systemDBFileRelativePathFragments[systemDBFileRelativePathFragments.length - 1] + ".mv.db";
+        Path sourceFile = Paths.get("database", systemDBFileName);
+        Path targetOldFile = Paths.get(oldDatabaseFolder.toString(), systemDBFileName);
+        Path targetNewFile = Paths.get(newDatabaseFolder.toString(), systemDBFileName);
+        LOGGER.info("Copy current system database to " + oldDatabaseFolder.toString());
+        Files.copy(sourceFile, targetOldFile);
+        LOGGER.info("Copy current system database to " + newDatabaseFolder.toString());
+        Files.copy(sourceFile, targetNewFile);
     }
 }
--- irontest-core-server/src/main/java/io/irontest/upgrade/UpgradeCommand.java
+++ irontest-core-server/src/main/java/io/irontest/upgrade/UpgradeCommand.java
@@ -9,7 +9,7 @@ import net.sourceforge.argparse4j.inf.Namespace;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.jdbi.v3.core.Jdbi;
 
-import java.util.Scanner;
+import java.io.IOException;
 
 public class UpgradeCommand extends ConfiguredCommand<IronTestConfiguration> {
 
@@ -18,55 +18,39 @@ public class UpgradeCommand extends ConfiguredCommand<IronTestConfiguration> {
     }
 
     @Override
-    protected void run(Bootstrap bootstrap, Namespace namespace, IronTestConfiguration configuration) {
-        String systemDatabaseVersionStr = getSystemDatabaseVersionStr(configuration);
+    protected void run(Bootstrap bootstrap, Namespace namespace, IronTestConfiguration configuration) throws IOException {
+        String systemDBVersionStr = getSystemDBVersionStr(configuration);
         String jarFileVersionStr = Version.VERSION;
         DefaultArtifactVersion jarFileVersion = new DefaultArtifactVersion(jarFileVersionStr);
-        DefaultArtifactVersion systemDatabaseVersion = new DefaultArtifactVersion(systemDatabaseVersionStr);
-        int result = systemDatabaseVersion.compareTo(jarFileVersion);
-        if ("SNAPSHOT".equals(systemDatabaseVersion.getQualifier())) {
-            System.out.println("System database version " + systemDatabaseVersionStr + " is a SNAPSHOT version. Upgrade is not supported.");
+        DefaultArtifactVersion systemDBVersion = new DefaultArtifactVersion(systemDBVersionStr);
+        int result = systemDBVersion.compareTo(jarFileVersion);
+        if ("SNAPSHOT".equals(systemDBVersion.getQualifier())) {
+            System.out.println("System database version " + systemDBVersionStr + " is a SNAPSHOT version. Upgrade is not supported.");
         } else if ("SNAPSHOT".equals(jarFileVersion.getQualifier())) {
             System.out.println("Jar file version " + jarFileVersionStr + " is a SNAPSHOT version. Upgrade is not supported.");
         } else if (result == 0) {
             System.out.println("System database and the jar file are of the same version, so no need to upgrade.");
         } else if (result > 0) {    //  system database version is bigger
-            System.out.println("The system database version " + systemDatabaseVersionStr + " is bigger than the jar file version. Please");
+            System.out.println("The system database version " + systemDBVersionStr + " is bigger than the jar file version. Please");
             System.out.println("  download and build the latest version of Iron Test,");
             System.out.println("  copy the jar file from the dist folder to your current <IronTest_Home> directory, and");
             System.out.println("  start the new version of Iron Test in your current <IronTest_Home> (like by running the start.bat).");
         } else {    //  system database version is smaller
             UpgradeActions upgradeActions = new UpgradeActions();
-            if (upgradeActions.needsSystemDBUpgrade(systemDatabaseVersion, jarFileVersion)) {
-                System.out.println("Please manually backup <IronTest_Home>/database folder to your normal maintenance backup location. Type y and then Enter to confirm backup completion.");
-                Scanner scanner = new Scanner(System.in);
-                String line = null;
-                while (!"y".equalsIgnoreCase(line)) {
-                    line = scanner.nextLine().trim();
-                }
-            }
-
-            if (upgradeActions.needsConfigYmlUpgrade(systemDatabaseVersion, jarFileVersion)) {
-                System.out.println("Please manually backup <IronTest_Home>/config.yml file to your normal maintenance backup location. Type y and then Enter to confirm backup completion.");
-                Scanner scanner = new Scanner(System.in);
-                String line = null;
-                while (!"y".equalsIgnoreCase(line)) {
-                    line = scanner.nextLine().trim();
-                }
-            }
+            upgradeActions.upgrade(systemDBVersion, jarFileVersion, configuration);
         }
     }
 
-    private String getSystemDatabaseVersionStr(IronTestConfiguration configuration) {
-        DataSourceFactory systemDatabaseConfiguration = configuration.getSystemDatabase();
-        String systemDatabaseURL = systemDatabaseConfiguration.getUrl();
-        String systemDatabaseBaseURL = systemDatabaseURL.split(";")[0];
-        Jdbi jdbi = Jdbi.create(systemDatabaseBaseURL + ";IFEXISTS=TRUE",
-                systemDatabaseConfiguration.getUser(), systemDatabaseConfiguration.getPassword());
-        String systemDatabaseVersionStr = jdbi.withHandle(handle ->
+    private String getSystemDBVersionStr(IronTestConfiguration configuration) {
+        DataSourceFactory systemDBConfiguration = configuration.getSystemDatabase();
+        String systemDBURL = systemDBConfiguration.getUrl();
+        String systemDBBaseURL = systemDBURL.split(";")[0];
+        Jdbi jdbi = Jdbi.create(systemDBBaseURL + ";IFEXISTS=TRUE",
+                systemDBConfiguration.getUser(), systemDBConfiguration.getPassword());
+        String systemDBVersionStr = jdbi.withHandle(handle ->
                 handle.createQuery("select version from version")
                         .mapTo(String.class)
                         .findOnly());
-        return systemDatabaseVersionStr;
+        return systemDBVersionStr;
     }
 }
