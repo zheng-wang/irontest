0eb6d7afc150c9d3088bb4178a6f438ffd57eac9
Zheng Wang <zheng-wang@users.noreply.github.com>
2021-02-07 19:28:59 +0800

    JMS test step

--- irontest-core-ui/src/main/resources/assets/app/js/teststeps/jms-teststep-action-controller.js
+++ irontest-core-ui/src/main/resources/assets/app/js/teststeps/jms-teststep-action-controller.js
@@ -3,8 +3,8 @@
 //  NOTICE:
 //    The $scope here prototypically inherits from the $scope of TeststepsActionController.
 //    ng-include also creates a scope.
-angular.module('irontest').controller('JMSTeststepActionController', ['$scope', '$timeout',
-  function($scope, $timeout) {
+angular.module('irontest').controller('JMSTeststepActionController', ['$scope', '$timeout', 'IronTestUtils', 'Teststeps',
+  function($scope, $timeout, IronTestUtils, Teststeps) {
     var timer;
     $scope.steprun = {};
 
@@ -28,6 +28,44 @@ angular.module('irontest').controller('JMSTeststepActionController', ['$scope', 
 
       //  update test step immediately (no timeout)
       $scope.update(isValid);
+    };
+
+    $scope.endpointInfoIncomplete = function() {
+      var endpoint = $scope.teststep.endpoint;
+      var endpointOtherProperties = endpoint.otherProperties;
+      return !endpointOtherProperties.jmsProvider ||
+        (endpointOtherProperties.jmsProvider === 'Solace' && (!endpoint.host || !endpoint.port ||
+          !endpointOtherProperties.vpn));
+    };
+
+    $scope.actionInfoIncomplete = function() {
+      var teststep = $scope.teststep;
+      if (!teststep.action) {
+        return true;
+      } else if (teststep.otherProperties.destinationType === 'Queue') {
+        return !teststep.otherProperties.queueName;
+      } else if (teststep.otherProperties.destinationType === 'Topic') {
+        return !teststep.otherProperties.topicString;
+      } else {
+        return true;
+      }
+    };
+
+    $scope.doAction = function() {
+      clearPreviousRunStatus();
+
+      var teststep = new Teststeps($scope.teststep);
+      $scope.steprun.status = 'ongoing';
+      teststep.$run(function(basicTeststepRun) {
+        $scope.steprun.response = basicTeststepRun.response;
+        $scope.steprun.status = 'finished';
+        timer = $timeout(function() {
+          $scope.steprun.status = null;
+        }, 15000);
+      }, function(error) {
+        $scope.steprun.status = 'failed';
+        IronTestUtils.openErrorHTTPResponseModal(error);
+      });
     };
   }
 ]);
